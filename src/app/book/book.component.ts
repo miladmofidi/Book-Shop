618b9799d15962044c0f9969d45fc6acb212e401
Milad Mofidi <milad.mofidi@gmail.com>
2023-07-05 15:04:12 +0100

    adding book component

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,20 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Book} from "../interface/Book";
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css']
+})
+export class BookComponent implements OnInit{
+  @Input() book: Book={} as Book;
+  @Output() bookEmitter = new EventEmitter<Book>();
+  constructor() {
+  }
+  ngOnInit(): void {
+  }
+
+  addToCard() {
+    this.bookEmitter.emit(this.book)
+  }
+}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Book} from "../interface/Book";
 
 @Component({
   selector: 'app-books',
@@ -7,16 +8,42 @@ import {Component, OnInit} from '@angular/core';
 })
 export class BooksComponent implements OnInit{
 
-  name1: string= "Clean Code";
-  author1: string="Robert C. Martin";
-  src1: string="https://m.media-amazon.com/images/I/41xShlnTZTL._SY466_.jpg";
 
-  name2: string= "Pragmatic Programmer";
-  author2: string="David Thomas";
-  src2: string="https://m.media-amazon.com/images/I/71f743sOPoL._SY466_.jpg";
+  books: Book[] = [
+    {
+      name:  "clean code",
+      author: "robert c. martin",
+      image: "https://m.media-amazon.com/images/I/41xShlnTZTL._SY466_.jpg",
+      amount: 24.99,
+    },
+    {
+      name:  "pragmatic programmer",
+      author: "david thomas",
+      image: "https://m.media-amazon.com/images/I/71f743sOPoL._SY466_.jpg",
+      amount: 19.99,
+    },
+    {
+      name:  "The Art of Computer Programming: Volume 1: Fundamental Algorithms",
+      author: "Donald E. Knuth",
+      image: "https://m.media-amazon.com/images/I/71++Mc6LvEL._SY466_.jpg",
+      amount: 39.99,
+    },
+    {
+      name:  "The Clean Coder: A Code of Conduct for Professional Programmers",
+      author: "Robert C. Martin",
+      image: "https://m.media-amazon.com/images/I/51uO-K+V5dL._SY466_.jpg",
+      amount: 20.99,
+    },
+    {
+      name:  "JavaScript from Beginner to Professional:",
+      author: "Laurence Lars Svekis",
+      image: "https://m.media-amazon.com/images/I/71oZ45OrLjL._SY466_.jpg",
+      amount: 17.99,
+    }
 
-  myName: string='';
+  ]
   isShowing: boolean = true;
+  card: Book[]= [];
 
   ngOnInit(): void {
   }
@@ -25,4 +52,9 @@ export class BooksComponent implements OnInit{
   toggleBooks() {
     this.isShowing = !this.isShowing;
   }
+
+  addToCard(book: Book) {
+
+  }
 }
+
