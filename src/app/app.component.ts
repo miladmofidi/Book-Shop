f760da7a108179fcffffe8f8fa1265bd3df9900a
Milad Mofidi <milad.mofidi@gmail.com>
2023-07-07 16:14:22 +0100

    adding some styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,22 +4,24 @@ import {firebaseConfig} from "./firebase.config";
 import {AuthService} from "./auth/auth.service";
 import {CartService} from "./services/cart.service";
 import {Book} from "./interface/Book";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
   title = 'Book Shop';
 
-  constructor(private authService: AuthService, private cartService: CartService) {
+  constructor(private authService: AuthService, private cartService: CartService, private router: Router) {
   }
 
   ngOnInit(): void {
     initializeApp(firebaseConfig);
   }
+
   isAuthenticated() {
     return this.authService.isAuthenticated;
   }
@@ -31,4 +33,13 @@ export class AppComponent implements OnInit{
   getCart(): Book[] {
     return this.cartService.get();
   }
+
+  onCartClick(): void {
+    if (!this.isAuthenticated()) {
+      alert('To see your Cart please login first');
+    } else {
+      this.router.navigate(['/cart']);
+    }
+  }
+
 }
